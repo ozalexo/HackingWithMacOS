ee3f0184712a7ac3369981105cacddbed9855700
Aleksey Ozerov <alexozerov@gmail.com>
2020-02-04 11:02:18 +0300

    Created new Xcode project

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Aleksey Ozerov on 04.02.2020.
+//  Copyright © 2020 Aleksey Ozerov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
