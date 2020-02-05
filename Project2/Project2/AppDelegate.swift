a68166c88eee1b35e622536aef068369cd237a60
Aleksey Ozerov <alexozerov@gmail.com>
2020-02-05 19:17:00 +0300

    Created new Xcode project

---
+++ Project2/Project2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project2
+//
+//  Created by Aleksey Ozerov on 05.02.2020.
+//  Copyright © 2020 Aleksey Ozerov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
